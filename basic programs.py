7953a80fb41e913b2c8a20dedbef87abfbdab992
Vedanshi Dubey <131689978+Vedanshidubey2005@users.noreply.github.com>
2023-08-23 13:33:48 +0530

    Add files via upload

---
+++ basic programs.py
@@ -0,0 +1,18 @@
+         #Q1
+# print('''Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.''')
+          #Q2
+# from playsound import playsound
+# playsound('C:\\Users\Vedanshi Dubey\\Documents\\Sound recordings//play.mp3')
+          #Q3
+# import os
+# print(os.listdir())        #print all files of this given folder
+          #Q4
+#a= "Harry"
+#a ='Harry'
+# a = '''Harry
+# has a
+# youtube channel.'''
+# print(a)
